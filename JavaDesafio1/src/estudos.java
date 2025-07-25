5a710a45c6e96dd21dd26da7f002d61a2eeed080
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-07-25 11:13:10 -0300

    Add files via upload

---
+++ JavaDesafio1/src/estudos.java
@@ -0,0 +1,6 @@
+public class estudos {
+    public static void main(String[] args){
+        System.out.println("JVM - è uma ferramenta muito importante no mundo java.");
+        System.out.println("JRE - Usado para consumidores.");
+    }
+}
