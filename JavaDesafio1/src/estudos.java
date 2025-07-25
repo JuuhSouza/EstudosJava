56c6767991b2c0bba48fd7d3a07dd633d250ddb3
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-07-25 11:05:36 -0300

    Delete JavaDesafio1 directory

--- JavaDesafio1/src/estudos.java
+++
@@ -1,6 +0,0 @@
-public class estudos {
-    public static void main(String[] args){
-        System.out.println("JVM - è uma ferramenta muito importante no mundo java.");
-        System.out.println("JRE - Usado para consumidores.");
-    }
-}
