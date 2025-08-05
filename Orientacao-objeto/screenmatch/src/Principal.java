6af4d0e0bbc57d2aa27c85d005a9533b55dcdda6
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-08-05 11:24:47 -0300

    Add files via upload

--- Orientacao-objeto/screenmatch/src/Principal.java
+++ Orientacao-objeto/screenmatch/src/Principal.java
@@ -1,28 +1,40 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Wall-e";
-        meuFilme.anoLancamento = 2008;
-        meuFilme.duracaoMinutos = 200;
+        //nova maneira de escrever o codigo
+        meuFilme.setNome("Wall-e");
+        meuFilme.setAnoLancamento(2008);
+        meuFilme.setDuracaoMinutos(180);
 
         meuFilme.fichaTecnica();
         meuFilme.avalia(10);
         meuFilme.avalia(8);
-        System.out.println("Avaliações: " + meuFilme.avaliacao);
-        System.out.println("Total de avaliações: " + meuFilme.totalAvaliacao);
+        //System.out.println("Avaliações: " + meuFilme.avaliacao);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalAvaliacao());
         System.out.println("Média de avaliações: " + meuFilme.mediaAvaliacao());
 
-        Filme meuFilme2 = new Filme();
-        meuFilme2.nome = "up";
-        meuFilme2.duracaoMinutos = 200;
-        meuFilme2.anoLancamento = 2005;
+        //jeito antigo de se fazer
 
-        meuFilme2.fichaTecnica();
-        meuFilme2.avalia(9);
-        meuFilme2.avalia(6);
-        meuFilme2.avalia(10);
-        System.out.println("Avaliações: " + meuFilme2.avaliacao);
-        System.out.println("Total de avaliações: " + meuFilme2.totalAvaliacao);
-        System.out.println("Média de avaliações: " + meuFilme2.mediaAvaliacao());
+       // Filme meuFilme2 = new Filme();
+       // meuFilme2.nome = "up";
+       // meuFilme2.duracaoMinutos = 200;
+       // meuFilme2.anoLancamento = 2005;
+
+
+       // meuFilme2.fichaTecnica();
+       // meuFilme2.avalia(9);
+       // meuFilme2.avalia(6);
+      //  meuFilme2.avalia(10);
+      //  System.out.println("Total de avaliações: " + meuFilme2.getTotalAvaliacao());
+      //  System.out.println("Média de avaliações: " + meuFilme2.mediaAvaliacao());
+
+        //Jeito antigo de se fazer
+        //meuFilme2.avaliacao = 10;
+        //meuFilme2.totalAvaliacao =1;
+       //System.out.println(meuFilme2.mediaAvaliacao());
+
     }
 }
+
