f60f31c312c47fd6d14226e44bfd3bce4a843b70
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-08-11 10:19:59 -0300

    Add files via upload

---
+++ Orientacao-objeto/projetoFinal/src/br/com/alura/minhasmusicas/Audio.java
@@ -0,0 +1,10 @@
+package br.com.alura.minhasmusicas;
+
+public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private double classificacao;
+
+
+}
