e3ab7f185b438cc8e99e6a437d6487f43310bac7
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-09-01 11:02:55 -0300

    Add files via upload

---
+++ listas/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculo.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+    private String diretor;
+
+    public String getDiretor(){
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return getTotalAvaliacao() / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome();
+    }
+
+}
