c5f3ec506ca29146c319209ca44e397ee7363d15
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-09-23 11:55:30 -0300

    Add files via upload

---
+++ API/screenmatch-API/src/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,77 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+//        meuFilme.setNome("O poderoso chefão");
+//        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie("Lost",2000);
+//        lost.setNome("Lost");
+//        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme("Avatar",2023);
+//        outroFilme.setNome("Avatar");
+//        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
+        Filme filmeDaJulia = new Filme("Familia Addams",2024);
+//        filmeDaJulia.setNome("Familia Addams");
+//        filmeDaJulia.setAnoDeLancamento(2024);
+        filmeDaJulia.setDuracaoEmMinutos(200);
+        filmeDaJulia.avalia(8);
+
+        ArrayList<Filme> listaDeFilme = new ArrayList<>();
+        listaDeFilme.add(filmeDaJulia);
+        listaDeFilme.add(meuFilme);
+        listaDeFilme.add(outroFilme);
+        System.out.println("Tamanho da lista " + listaDeFilme.size());
+        System.out.println("Primeiro filme " + listaDeFilme.get(0).getNome());
+
+        System.out.println("toString do filme " + listaDeFilme.get(0).toString());
+
+
+
+
+    }
+}
