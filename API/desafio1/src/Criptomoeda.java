202e9976f02c66c8f40934d4f0bb14cf82fa0e93
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-09-25 20:17:32 -0300

    Update Criptomoeda.java

--- API/desafio1/src/Criptomoeda.java
+++ API/desafio1/src/Criptomoeda.java
@@ -16,11 +16,12 @@ public class Criptomoeda {
         String busca = "https://api.coingecko.com/api/v3/simple/price?ids=" + criptomoeda + "&vs_currencies=usd";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(""))
+                .uri(URI.create(busca))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
     }
 }
+
 
