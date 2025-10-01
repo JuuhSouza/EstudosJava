406a8b4a81972382b99f346e43ce3b17d395c94b
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-10-01 10:23:39 -0300

    Add files via upload

---
+++ API/screenmatch-API/src/br/com/alura/screenmatch/modelos/TituloOmdb.java
@@ -0,0 +1,5 @@
+package br.com.alura.screenmatch.modelos;
+
+public record TituloOmdb(String title, String year, String runtime) {
+    //não serapreciso escrevenesta classe, ela é uma classe especial q ja tem tudo
+}
--- API/screenmatch-API/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ API/screenmatch-API/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -21,8 +27,21 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
 
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy
+                        .UPPER_CAMEL_CASE).create(); // usamos para n precisar colocar letra miuscula na classe TituloOmdb para manter o pradrão
+
+        //Titulo meuTitulo = gson.fromJson(json, Titulo.class); jeito antigo
+
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println("Titulo já convertido");
+        System.out.println(meuTitulo);
 
 
     }
