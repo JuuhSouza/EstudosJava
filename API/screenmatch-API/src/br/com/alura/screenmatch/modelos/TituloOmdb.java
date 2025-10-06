d862c0b4e885e10acaf0355bac15a47f54358c7e
Júlia de Souza <146949046+JuuhSouza@users.noreply.github.com>
2025-10-06 11:32:46 -0300

    Delete API/screenmatch-API directory

--- API/screenmatch-API/src/br/com/alura/screenmatch/modelos/TituloOmdb.java
+++
@@ -1,5 +0,0 @@
-package br.com.alura.screenmatch.modelos;
-
-public record TituloOmdb(String title, String year, String runtime) {
-    //não sera preciso escreve nesta classe, ela é uma classe especial q ja tem tudo
-}
